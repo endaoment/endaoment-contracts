f262aecabf088e117e9a015ac41a870150dc2217
Liam Heeger <lheeger@u.rochester.edu>
2020-06-15 16:09:36 -0400

    Adding coverage support to buidler config

--- buidler.config.js
+++ buidler.config.js
@@ -1,4 +1,5 @@
 usePlugin("@nomiclabs/buidler-truffle5");
+usePlugin("solidity-coverage");
 
 task("accounts", "Prints the list of accounts", async () => {
   const accounts = await web3.eth.getAccounts();
@@ -9,7 +10,7 @@ task("accounts", "Prints the list of accounts", async () => {
 });
 
 module.exports = {
-  defaultNetwork: "buidlerevm",
+  defaultNetwork: "rinkeby",
   networks: {
     mainnet: {
       url: `https://mainnet.infura.io/v3/${process.env.INFURA_KEY}`,
@@ -20,6 +21,9 @@ module.exports = {
       url: `https://rinkeby.infura.io/v3/${process.env.INFURA_KEY}`,
       chainId: 4,
       accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : "remote",
+    },
+    coverage: {
+      url: 'http://localhost:8555'
     },
   },
 };
