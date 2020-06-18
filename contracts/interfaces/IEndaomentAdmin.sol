2e374dc05121b5db47a283dc3e9d5813e6633ca9
rheeger <rheeger@gmail.com>
2020-06-18 17:10:47 -0400

    MIT MIT MIT MIT

--- contracts/interfaces/IEndaomentAdmin.sol
+++ contracts/interfaces/IEndaomentAdmin.sol
@@ -1,7 +1,10 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 
 pragma solidity ^0.6.10;
 
+/**
+ * @dev Interface of the EndaomentAdmin contract
+ */
 interface IEndaomentAdmin {
   event RoleModified(Role indexed role, address account);
   event RolePaused(Role indexed role);
