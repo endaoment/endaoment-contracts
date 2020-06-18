d1931e517e271cc5733b23676779fdf74da9be17
rheeger <rheeger@gmail.com>
2020-06-18 18:20:44 -0400

    updating license

--- contracts/Administratable.sol
+++ contracts/Administratable.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: BSD 3-Clause
 
 pragma solidity ^0.6.10;
 
--- contracts/mocks/ERC20Mock.sol
+++ contracts/mocks/ERC20Mock.sol
@@ -1,4 +1,5 @@
-//  SPDX-License-Identifier: MIT
+//  SPDX-License-Identifier: BSD 3-Clause
+
 pragma solidity ^0.6.10;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
