7bc8ae61ad0056ec61e3375a4cba9778336190f9
rheeger <rheeger@gmail.com>
2020-09-22 10:53:28 -0700

    ready for PR

--- tokenUnlock.js
+++ tokenUnlock.js
@@ -12,11 +12,16 @@ const infuraEndpoint = "https://" + infuraPrefix + ".infura.io/v3/" + infuraKey;
 const provider = new HDWalletProvider(mnemonic, infuraEndpoint);
 const web3 = new Web3(provider);
 
-const tokensToUnlock = []
-
+const tokensToUnlock = [{
+  "tokenAddress": "0x8ae2a0bfb3315b63ee8e88ac7d3f6b5a68f01cf5",
+  "tokenDecimals": 18,
+  "tokenSymbol": "SNX",
+  "tokenName": "SNX",
+}]
 const unlockToken = async (tokenContractAddress) => {
+  const nonce = await web3.eth.getTransactionCount(accounts[0])
   const tokenContract = await new web3.eth.Contract(ERC20.abi, tokenContractAddress)
-  const receipt = await tokenContract.methods.approve("0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D", "115792089237316195423570985008687907853269984665640564039457584007913129639935").send({ from: accountant })
+  const receipt = await tokenContract.methods.approve("0xf164fC0Ec4E93095b804a4795bBe1e041497b92a", "115792089237316195423570985008687907853269984665640564039457584007913129639935").send({ from: accountant, nonce, gasPrice: web3.utils.toWei("545", "gwei") })
 
   return receipt
 }
@@ -28,11 +33,13 @@ const deploy = async () => {
   console.log("Network: " + process.env.INFURA_PREFIX);
   console.log("Account: " + accounts[0]);
 
+
+
   await tokensToUnlock.map(async (token) => {
     try {
       console.log("Unlocking " + token.tokenSymbol)
       const unlock = await unlockToken(token.tokenAddress)
-      return console.log("Unlocked " + token.tokenSymbol + unlock)
+      return console.log("Unlocked " + token.tokenSymbol, unlock)
     }
     catch (err) {
       console.log(err)
