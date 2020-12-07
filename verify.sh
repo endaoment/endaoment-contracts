2cc1aed3e3f32c4d99d5ad71a7b3d8cd03794ace
wildmolasses <changes@gmail.com>
2020-12-07 11:42:43 -0600

    use npx

--- verify.sh
+++ verify.sh
@@ -1,3 +1,3 @@
-hardhat verify --network mainnet 0x2a108b1810890482fdb3be188616c56dd0170a8e # admin
-hardhat verify --network mainnet 0x59bf396e21d8e941415ae80ab7347bdf82d22d6e "0x2a108b1810890482fdb3be188616c56dd0170a8e" # FundFactory
-hardhat verify --network mainnet 0xf2f5bb5d80413dd1feea6254786465109adf4b9e "0x2a108b1810890482fdb3be188616c56dd0170a8e" # OrgFactory
+npx hardhat verify --network mainnet 0x2a108b1810890482fdb3be188616c56dd0170a8e # admin
+npx hardhat verify --network mainnet 0x59bf396e21d8e941415ae80ab7347bdf82d22d6e "0x2a108b1810890482fdb3be188616c56dd0170a8e" # FundFactory
+npx hardhat verify --network mainnet 0xf2f5bb5d80413dd1feea6254786465109adf4b9e "0x2a108b1810890482fdb3be188616c56dd0170a8e" # OrgFactory
