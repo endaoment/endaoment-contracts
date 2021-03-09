131ad5a00a37b1024c114e6a538d9f255b0b0102
rheeger <rheeger@gmail.com>
2021-03-08 21:51:51 -0700

    can't get unlock to work for some reason, pushing so @wildmolasses can check it out

--- tokenUnlock.js
+++ tokenUnlock.js
@@ -14,16 +14,28 @@ const web3 = new Web3(provider);
 
 const tokensToUnlock = [
   {
-    "tokenAddress": "",
+    "tokenAddress": "0x07865c6E87B9F70255377e024ace6630C1Eaa37F",
+    "tokenDecimals": 6,
+    "tokenSymbol": "USDC",
+    "tokenName": "USD//C",
+  },
+  {
+    "tokenAddress": "0xad6d458402f60fd3bd25163575031acdce07538d",
     "tokenDecimals": 18,
-    "tokenSymbol": "",
-    "tokenName": "",
+    "tokenSymbol": "DAI",
+    "tokenName": "DAI",
+  },
+  {
+    "tokenAddress": "0x8ae2a0bfb3315b63ee8e88ac7d3f6b5a68f01cf5",
+    "tokenDecimals": 18,
+    "tokenSymbol": "SNX",
+    "tokenName": "SNX",
   },
 ]
 
-const unlockToken = async (tokenContractAddress) => {
+const unlockToken = async (tokenContractAddress, account) => {
   const tokenContract = await new web3.eth.Contract(ERC20.abi, tokenContractAddress)
-  const receipt = await tokenContract.methods.approve("0x7a250d5630b4cf539739df2c5dacb4c659f2488d", "115792089237316195423570985008687907853269984665640564039457584007913129639935").send({ from: accountant })
+  const receipt = await tokenContract.methods.approve("0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506", "115792089237316195423570985008687907853269984665640564039457584007913129639935").send({ from: account, chainId: process.env.CHAINID })
 
   return receipt
 }
@@ -40,11 +52,11 @@ const deploy = async () => {
   await tokensToUnlock.map(async (token) => {
     try {
       console.log("Unlocking " + token.tokenSymbol)
-      const unlock = await unlockToken(token.tokenAddress)
+      const unlock = await unlockToken(token.tokenAddress, accounts[0])
       return console.log("Unlocked " + token.tokenSymbol, unlock)
     }
     catch (err) {
-      console.log(err)
+      console.log(`${token.tokenSymbol} ERROR:`, err)
     }
     return
   })
